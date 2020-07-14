142271210312d94f66b8c17aaf35161e26d143be
kobayashi1006 <67186757+kobayashi1006@users.noreply.github.com>
2020-07-14 22:57:43 +0900

    課題提出

---
+++ Javastudy/test/src/test/SumMain.java
@@ -0,0 +1,10 @@
+package test;
+
+public class SumMain {
+
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+        int sumValue = sum.plus(1, 2, 3);
+        System.out.println(sumValue);
+    }
+}
